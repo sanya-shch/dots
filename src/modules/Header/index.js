991d1093b6ad74192353e038c17b92cc2f9af4a4
sanya-shch <shchipkov.sanya@ukr.net>
2023-01-04 22:43:43 +0200

    updated header

--- src/modules/Header/index.js
+++ src/modules/Header/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { observer } from "mobx-react-lite";
 
 import "./style.scss";
 
 import gameStore from "../../store/gameStore";
-import { observer } from "mobx-react-lite";
+import { ReactComponent as ResetSvg } from "../../assets/remove-icon.svg";
+import { reset } from "../../firebase/gameRooms";
 
-const Header = observer(() => {
+const Header = observer((id) => {
   const navigate = useNavigate();
 
   const currentPlayerColor = gameStore.playersList.find(
     (item) => item.uid === gameStore.currentPlayerUid
   ).color;
+
+  const handleClickReset = async () => {
+    await reset(id, gameStore.playersList[0].uid);
+  };
 
   return (
     <div
@@ -19,19 +25,27 @@ const Header = observer(() => {
         gameStore.ongoingGame ? currentPlayerColor : ""
       }`}
     >
-      <div className="title" onClick={() => navigate("/")}>
-        DOTS
-      </div>
-
-      {gameStore.ongoingGame && (
-        <div className="players_list">
-          {gameStore.playersList.map((item) => (
-            <div key={item.color} className={`player_item ${item.color}`}>
-              {item.username}
-            </div>
-          ))}
+      <div className="header_wrapper">
+        <div className="title" onClick={() => navigate("/")}>
+          DOTS
         </div>
-      )}
+
+        {gameStore.ongoingGame && (
+          <div className="players_list">
+            {gameStore.playersList.map((item) => (
+              <div key={item.color} className={`player_item ${item.color}`}>
+                <span>{item.username}</span>
+              </div>
+            ))}
+
+            <div className="info_block">
+              {gameStore.isHost && gameStore.ongoingGame && (
+                <ResetSvg onClick={handleClickReset} />
+              )}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 });
